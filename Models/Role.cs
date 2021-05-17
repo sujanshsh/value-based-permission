4296c28131980fd35fec158aa98c62d332513394
Sujan Shakya <shakya.sujan@worldlink.com.np>
2021-05-18 04:48:38 +0545

    Adds Role model with table view

---
+++ Models/Role.cs
@@ -0,0 +1,10 @@
+namespace AccessControlAdmin.Models
+{
+
+    public class Role
+    {
+        public int id { get; set; } = 0;
+        public string name { get; set; } = "";
+        public string description { get; set; } = "";
+    }
+}
