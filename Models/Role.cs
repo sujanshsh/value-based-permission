d4c971607d3c38d71cfcb7a3322bc431b7faa90a
Sujan Shakya <shakya.sujan@worldlink.com.np>
2021-05-20 11:25:37 +0545

    Updates roles page and all logic

--- Models/Role.cs
+++ Models/Role.cs
@@ -6,5 +6,7 @@ namespace AccessControlAdmin.Models
         public int id { get; set; } = 0;
         public string name { get; set; } = "";
         public string description { get; set; } = "";
+        public string createdAt { get; set; } = "";
+        public string updatedAt { get; set; } = "";
     }
 }
