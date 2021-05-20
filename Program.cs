9f4f11aebd6a5dde716571f390256b9935eeff64
Sujan Shakya <shakya.sujan@worldlink.com.np>
2021-05-20 10:13:10 +0545

    Uses different Base Uri for HttpClient

--- Program.cs
+++ Program.cs
@@ -19,8 +19,9 @@ namespace AccessControlAdmin
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddSingleton<Services.AuthenticationService>();
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            // builder.Services.AddSingleton<Services.AuthenticationService>();
+            // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:3000") });
 
             builder.Services.AddOptions();
             builder.Services.AddAuthorizationCore();
