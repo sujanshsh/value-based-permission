754576d076d40dbee681f93c3ff13ec8933ada24
Sujan Shakya <shakya.sujan@worldlink.com.np>
2021-05-20 11:25:00 +0545

    Revert removal of authentication service provider

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace AccessControlAdmin
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            // builder.Services.AddSingleton<Services.AuthenticationService>();
+            builder.Services.AddSingleton<Services.AuthenticationService>();
             // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:3000") });
 
