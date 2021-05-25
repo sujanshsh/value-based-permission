bbcd6b0e5bb7f437d51ad5a61475855fe9a323ba
Sujan Shakya <shakya.sujan@worldlink.com.np>
2021-05-25 09:30:07 +0545

    Creates user role model

---
+++ Models/UserRole.cs
@@ -0,0 +1,10 @@
+namespace AccessControlAdmin.Models
+{
+
+    public class UserRole
+    {
+        public int id { get; set; } = 0;
+        public int user_id { get; set; } = 0;
+        public int role_id { get; set; } = 0;
+    }
+}
