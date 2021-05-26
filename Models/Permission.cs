e79a5acead4f4bb7819028b2a4f4ef63663b4804
Sujan Shakya <shakya.sujan@worldlink.com.np>
2021-05-26 15:27:30 +0545

    Updates the Permission view-model

--- Models/Permission.cs
+++ Models/Permission.cs
@@ -1,14 +1,21 @@
 namespace AccessControlAdmin.Models
 {
 
+    #nullable enable
+    
     public class Permission
     {
 
         public int id { get; set; } = 0;
         public string name { get; set; } = "";
+
+        public string? suffix { get; set; } = "";
         public string description { get; set; } = "";
+        public int? value_type_id { get; set; } = 0;
+        public string? value_type_name { get; set; } = "";
         public string values { get; set; } = "";
         public string createdAt { get; set; } = "";
         public string updatedAt { get; set; } = "";
+        
     }
 }
