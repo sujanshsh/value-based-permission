1fa96d97d9fa44fcfcae24044c1e4fe32cb18684
Sujan Shakya <shakya.sujan@worldlink.com.np>
2021-05-28 09:47:49 +0545

    Adds permissions of user page

--- Models/PermissionsOfUser.cs
+++ Models/PermissionsOfUser.cs
@@ -1,17 +1,20 @@
 namespace AccessControlAdmin.Models
 {
 
-    public class PermissionsOfRole
+    #nullable enable
+    public class PermissionsOfUser
     {
 
         public int id { get; set; } = 0;
         public string name { get; set; } = "";
-
         public string suffix { get; set; } = "";
         public string description { get; set; } = "";
+        public int? value_type_id { get; set; } = 0;
         public string values { get; set; } = "";
         public string createdAt { get; set; } = "";
         public string updatedAt { get; set; } = "";
-        public int role_permissions_id { get; set; } = 0;
+        public int role_id { get; set; } = 0;
+        public string role_name { get; set; } = "";
+        public string? value_type_name { get; set; } = "";
     }
 }
